71b88aa4bdf99d5a86879cf6d049740bf0cf3120
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-10-10 14:28:44 -0700

    add post route

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { urlencoded } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -80,6 +82,32 @@ app.delete('/api/candidate/:id', (req, res) => {
         res.json({
             message: 'successfully deleted',
             changes: this.changes
+        });
+    });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
         });
     });
 });
