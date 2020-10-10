856ffdd42c64fc196831fe3ad7e07659b3f7f42a
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-10-10 15:17:14 -0700

    added join

--- server.js
+++ server.js
@@ -19,42 +19,35 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name 
+        AS party_name 
+        FROM candidates 
+        LEFT JOIN parties 
+        ON candidates.party_id = parties.id`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
 
-// GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
-
-// Delete a candidate
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
-
-// // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function (err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// });
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+        AS party_name 
+        FROM candidates 
+        LEFT JOIN parties 
+        ON candidates.party_id = parties.id 
+        WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
